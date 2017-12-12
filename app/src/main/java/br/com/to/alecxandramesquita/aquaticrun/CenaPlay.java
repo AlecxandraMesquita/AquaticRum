c6fca06d1a7fa4343adf110bf524548d394137e5
AlecxandraMesquita <alecxandramesquita9@gmail.com>
2017-12-11 23:30:02 -0300

    correção de uns bugs

--- app/src/main/java/br/com/to/alecxandramesquita/aquaticrun/CenaPlay.java
+++ app/src/main/java/br/com/to/alecxandramesquita/aquaticrun/CenaPlay.java
@@ -22,8 +22,6 @@ public class CenaPlay extends AGScene {
     AGSprite imagemBarril = null;
     AGSprite imagemExplosao = null;
     Boolean estado = false;
-
-    AGSprite up, left, right, upOn, leftOn, rightOn = null;
     ArrayList<AGSprite> listaBarril = null;
     AGTimer tempoGeraBarril = null;
     AGTimer tempobarril = null;
@@ -41,7 +39,7 @@ public class CenaPlay extends AGScene {
 
     @Override
     public void init() {
-        //Chama a imagem
+        //cria a imagem cetando qtd linha/coluna
         imagemAgua = this.createSprite(R.mipmap.agua, 1, 1);
         imagemBarco = this.createSprite(R.mipmap.barco, 1, 1);
         imagemPrincesa = this.createSprite(R.mipmap.sofia_pedindo_ajuda, 1, 1);
@@ -49,9 +47,10 @@ public class CenaPlay extends AGScene {
         AGSoundManager.vrMusic.loadMusic("jogando.wav", true);
         AGSoundManager.vrMusic.play();
         // imagemBarril = this.createSprite(R.mipmap.barril,1,1);
+       //instancia o tempo que irá gera os berris
         tempoGeraBarril = new AGTimer(1500);
         tempobarril = new AGTimer(5000);
-        //instancia uma liata de barris
+        //instancia uma lista de barris para o AGSprite
         listaBarril = new ArrayList<AGSprite>();
 
         //inatancia o tamanho das imagens
@@ -60,7 +59,7 @@ public class CenaPlay extends AGScene {
         imagemPrincesa.setScreenPercent(10, 12);
         // imagemBarril.setScreenPercent(20,10);
 
-        //pega imagem e a coloca no centro da tela
+        //pega imagem e a posiciona na tela
         imagemAgua.vrPosition.setXY(AGScreenManager.iScreenWidth / 2, AGScreenManager.iScreenHeight / 2);
         imagemBarco.vrPosition.setXY(AGScreenManager.iScreenWidth / 2, imagemBarco.vrPosition.getY() + 180);
         imagemPrincesa.vrPosition.setXY(AGScreenManager.iScreenWidth / 2, imagemPrincesa.vrPosition.getY() + 1800);
@@ -97,41 +96,43 @@ public class CenaPlay extends AGScene {
 
     }
 
-    //Cria a funcão que volta para  o menu
+    //metodo Cria a funcão que volta para  o menu
     public void voltar() {
+        //verifica se houve um toc no botão de voltar do aparelho
         if (AGInputManager.vrTouchEvents.backButtonClicked()) {
             AGSoundManager.vrMusic.stop();
+            //chama a próxima cena nunca esquecer do returne se não dará erro a execução
             this.vrGameManager.setCurrentScene(1);
             return;
 
         }
     }
 
-    //cria a função demovimentação do barco
+    //metodo cria a função demovimentação do barco
     public void barcoAnda() {
-        //movimeta a po
+        //movimeta a barco de acordo como sua posição em relacção a x e y
         imagemBarco.vrPosition.setX(imagemBarco.vrPosition.getX() + AGInputManager.vrAccelerometer.getAccelX());
         imagemBarco.vrPosition.setY(imagemBarco.vrPosition.getY() + AGInputManager.vrAccelerometer.getAccelY());
 
     }
 
-    /*método que cria os meteóros de forma aleatórias na tela*/
+    /*método que cria os barril de forma aleatórias na tela*/
     public void geraBarril() {
         tempoGeraBarril.update();
-        for (AGSprite barrilRecycled : listaBarril) {
-            //Se o meteóro for reciclado
-            if (barrilRecycled.bRecycled) {
+        for (AGSprite barrilRecicla : listaBarril) {
+            //Se o barril for reciclado
+            if (barrilRecicla.bRecycled) {
                 //Posição dele em X e Y será setada
-                barrilRecycled.vrPosition.setXY(50 + (int) (Math.random() * (AGScreenManager.iScreenWidth - 50)),
+                barrilRecicla.vrPosition.setXY(50 + (int) (Math.random() * (AGScreenManager.iScreenWidth - 50)),
                         AGScreenManager.iScreenHeight);
                 // E ele será setado como não reciclado
-                barrilRecycled.bRecycled = false;
+                barrilRecicla.bRecycled = false;
                 return;
-            }//Fim if que pergunta se o meteóro ja é reciclado
-        }//Fim for array de meteóros
+            }//Fim if que pergunta se o barril ja é reciclado
+        }//Fim for array de barris
 
         if (tempoGeraBarril.isTimeEnded()) {
-            //Cria o meteóro, posicion, dá tamanho e coloca ele no array de meteoros
+            //Cria o barrio, posição, dá tamanho e coloca ele no array de barris
             imagemBarril = createSprite(R.mipmap.barril, 1, 1);
             imagemBarril.setScreenPercent(13, 8);
             //Posição X é randomica com números entre 50 e tamanho da tela - 50
@@ -143,44 +144,46 @@ public class CenaPlay extends AGScene {
         }
     } //Fim metódo geraBarril
 
-    //metodo de atualizar o
+    //metodo de atualizar o barril
     public void atualizaBarril() {
         for (AGSprite imagemBarril : listaBarril) {
-            //Meteoro tera sua posição atual em y decrementada em 5 a cada frame
+            //barril tera sua posição atual em y decrementada em 5 a cada frame
             imagemBarril.vrPosition.setY(imagemBarril.vrPosition.getY() - 5);
-            //se a posição do meteóro em Y for menor do que a altura da tela
+            //se a posição do barril em Y for menor do que a altura da tela
             if (imagemBarril.vrPosition.getY() < 0) {
-                //Seta o meteóro para reciclado
+                //Seta o barril para reciclado
                 imagemBarril.bRecycled = true;
             }//Fim do if
         }//Fim do For
-    }//Fim do método atualizaMeteoro();,
+    }//Fim do metodo atualizabarril();,
 
     //Função de impaquito
     public void explode() {
 
-        for (AGSprite m : listaBarril) {
-            if (m.collide(imagemBarco) && m.bVisible) {
+        for (AGSprite barril : listaBarril) {
+            //se barril colidir no barco e estiver visivel
+            if (barril.collide(imagemBarco) && barril.bVisible) {
+                //chama a musica de explosão
                 AGSoundManager.vrMusic.loadMusic("explosao1.mp3", false);
                 AGSoundManager.vrMusic.play();
-                //instacia a imagem
+                //instacia a imagem de explosão
                 imagemExplosao = this.createSprite(R.mipmap.explosao, 1, 1);
-                //passao tamanho dela natela
+                //passao tamanho da imagem para a tela
                 imagemExplosao.setScreenPercent(30, 30);
-                //adiciona a animação
+                //adiciona a animação da imagem
                 imagemExplosao.addAnimation(20, false, 0, 15);
 
-                //faz a preparação para colisão
+                //ceta a posição da colisão de adordo com o xy dela em relação ao xy do barco
                 imagemExplosao.vrPosition.setXY(imagemBarco.vrPosition.getX(), imagemBarco.vrPosition.getY());
+                //ceta as dua imagns como falsa
                 imagemBarco.bVisible = false;
-                m.bVisible = false;
+                barril.bVisible = false;
                 return;
-
 
             }
         }
     }
-    //função vitória
+    //metodo com função vitória
     public void vitoria() {
         //verifiva a cgegada do barco na princesa
         if (imagemBarco.collide(imagemPrincesa)) {
@@ -192,18 +195,17 @@ public class CenaPlay extends AGScene {
         estado=false;
     }
 
-    //função derrota
+    //metodo com função derrota
     public void perde() {
         tempobarril.update();
         for (AGSprite barril : listaBarril) {
+            //verifica a colisao do varco com o barril ou se o tempo acabou
             if (imagemBarco.collide(barril)|| tempobarril.isTimeEnded()){
                //seta estado da variavel para verificar a função
                 estado = false;
                 this.vrGameManager.setCurrentScene(6);
                 return;
             }
-        }
-    }//Fim for array de meteóros
-
-
+        }//Fim for array de barris
+    }
 }
